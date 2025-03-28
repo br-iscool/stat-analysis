eb68e469be47c61c0dec9693a6e0bcfcdaf76315
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-03-28 14:09:10 -0600

    feat: percentage based stacked bar graph

---
+++ README.md
@@ -0,0 +1,3 @@
+# EPL Stat Analysis
+
+Stacked bar graph visualization of the percentage of home goals (FTHG) vs. away goals (FTAG) per team in the 2018-2019 English Premier League season.
--- main.py
+++ main.py
@@ -13,29 +13,28 @@ home_goals = df.groupby("HomeTeam")["FTHG"].sum()
 away_goals = df.groupby("AwayTeam")["FTAG"].sum()
 teams = sorted(set(home_goals.index) | set(away_goals.index))
 
-# Get list of total goals
-home_goals_list = []
-for team in teams:
-    home_goals_list.append(home_goals.get(team, 0))
-home_goals = home_goals_list
+# Calculate percentages
+home_goals_percentage = []
+away_goals_percentage = []
 
-away_goals_list = []
 for team in teams:
-    away_goals_list.append(away_goals.get(team, 0))
-away_goals = away_goals_list
+    home = home_goals.get(team, 0)
+    away = away_goals.get(team, 0)
+    total = home + away
+    
+    home_goals_percentage.append((home / total) * 100)
+    away_goals_percentage.append((away / total) * 100)
 
 # Plot bar graph
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 6), layout="constrained")
 
-plt.bar(teams, home_goals, color="orange", alpha=0.7, label="Home Goals")
-plt.bar(teams, away_goals, color="blue", alpha=0.5, label="Away Goals", bottom=home_goals)
+plt.bar(teams, home_goals_percentage, color="orange", alpha=0.7, label="Home Goals (%)")
+plt.bar(teams, away_goals_percentage, color="blue", alpha=0.5, label="Away Goals (%)", bottom=home_goals_percentage)
 
 plt.xlabel("Teams")
-plt.ylabel("Total Goals")
-plt.title("Home Goals vs. Away Goals (EPL 2018/19)")
+plt.ylabel("Percentage of Goals (%)")
+plt.title("Percentage of Home Goals vs. Away Goals (EPL 2018/19)")
 plt.legend()
 plt.xticks(rotation=45)
 
-plt.tight_layout()
 plt.show()
-
