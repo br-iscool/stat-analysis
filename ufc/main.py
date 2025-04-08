3dc2c6e5ea66384ddb7a154fa2fec4f761a42e71
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-04-07 20:03:16 -0600

    feat: ufc bar graph representation of number of KO / TKOs per round

--- ufc/main.py
+++ ufc/main.py
@@ -1,5 +1,22 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 
-plt.figure(figsize=(12, 6), layout="constrained")
+try:
+    df = pd.read_csv("ufc/ufc-master.csv")
+except FileNotFoundError:
+    print("Error: File not found")
+    exit()
+    
+finishes = df["Finish"].str.upper().str.strip()
+
+ko_finishes = df[finishes.str.contains("KO", na=False)].dropna(subset=["FinishRound"])
+ko_finishes["FinishRound"] = pd.to_numeric(ko_finishes["FinishRound"]).dropna().astype(int)
+
+total_kos = ko_finishes["FinishRound"].value_counts()
+
+plt.figure(figsize=(10, 6), layout="constrained")
+plt.bar(total_kos.index, total_kos.values, color="tomato")
+plt.xlabel("Round")
+plt.ylabel("Number of KOs / TKOs")
+plt.title("Average Number of KOs / TKOs per Round (Mid-2010 - Present)")
 plt.show()
